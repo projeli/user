77379f03032044bd73e9f67703ce698105341e89
DAQEM <daqemyt@gmail.com>
2025-02-14 15:48:20 +0100

    Basic User Service setup.

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers;
+
+[ApiController]
+[Route("users")]
+public class UserController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "Hello from UserService" });
+    }
+}
